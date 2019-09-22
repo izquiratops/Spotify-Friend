647acb042874a91e9681bb625189fcade07de590
Jordi Izquierdo <jicasares94@gmail.com>
2019-09-22 20:16:00 +0200

    This is working 100%

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 # Telegram-API
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
-# Spotipy
-import spotipy
-import spotipy.util as util
-import spotify_manager
-# Imported tokens/data from APIs
-import config
+
+import spoty, config, json
+
+def help(update, context):
+	message = "List of commands:\n/create_playlist\n/get_link\n\n(I'm not gonna explain this shit, this is so self-explanatory)"
+	update.message.reply_text(message)
+
+def create_playlist(update, context):
+	ret = spoty.show_playlists(spotify_token, config.spotify_username)
+	exist = False
+
+	for playlist in ret:
+		if config.playlist_name == playlist[1]:
+			message, exist = 'Already been created', True
+	if not exist:
+		spoty.create_playlist(spotify_token, config.playlist_name)
+		message = config.playlist_name + ' created successfully!'
+
+	context.bot.send_message(chat_id=update.message.chat_id, text=message)
+
+def get_link(update, context):
+	with open('playlist_return.json') as file:
+		data = json.load(file)
+		message = data['external_urls']['spotify']
+
+	if message:
+		context.bot.send_message(chat_id=update.message.chat_id, text=message)
+
+def read_message(update, context):
+	if 'open.spotify.com/' in update.message.text:
+		song_id = update.message.text.split('/')[-1].split('?si=')[0]
+	else:
+		return
+
+	ret = spoty.show_playlists(spotify_token, config.spotify_username)
+	for playlist in ret:
+		# playlist es un tuple de (ID, Name)
+		if config.playlist_name == playlist[1]:
+			spoty.add_song(spotify_token, playlist[0], song_id)
 
 # 'Updater' fetches new updates from telegram and passes them on to the 'Dispatcher'
 # 'use_context' arg to get better backwards compatibility
@@ -17,25 +50,12 @@ dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 					level=logging.INFO)
 
-spotify_token = util.prompt_for_user_token(username=config.app_name,\
-	scope='playlist-read-collaborative playlist-modify-public', client_id=config.SPOTIPY_CLIENT_ID,\
-	client_secret=config.SPOTIPY_CLIENT_SECRET, redirect_uri=config.SPOTIPY_REDIRECT_URI)
+spotify_token = spoty.get_token()
 
-def create_playlist(update, context):
-	spotify_manager.create_playlist(spotify_token, config.playlist_name)
-
-def read_message(update, context):
-	if 'open.spotify.com/' in update.message.text:
-		hash = update.message.text.split('/')[-1].split('?si=')[0]
-
-	playlists_list = spotify_manager.get_playlists(spotify_token, config.spotify_username)
-	for playlist in playlists_list:
-		if config.playlist_name in playlist[1]:
-			context.bot.send_message(chat_id=update.message.chat_id, text=playlist[1])
-
-start_handler = CommandHandler('create_playlist', create_playlist)
-read_handler = MessageHandler(Filters.text, read_message)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(read_handler)
+dispatcher.add_handler(CommandHandler('help', help))
+dispatcher.add_handler(CommandHandler('create_playlist', create_playlist))
+dispatcher.add_handler(CommandHandler('get_link', get_link))
+dispatcher.add_handler(MessageHandler(Filters.text, read_message))
 
 updater.start_polling()
+updater.idle()
--- spoty.py
+++ spoty.py
@@ -5,6 +5,13 @@ import json
 # Imported tokens/data from APIs
 import config
 
+def get_token():
+	return util.prompt_for_user_token(username=config.app_name,\
+				scope='playlist-read-collaborative playlist-modify-public',\
+				client_id=config.SPOTIPY_CLIENT_ID,\
+				client_secret=config.SPOTIPY_CLIENT_SECRET,\
+				redirect_uri=config.SPOTIPY_REDIRECT_URI)
+
 def create_playlist(spotify_token, playlist_name):
 	sp = spotipy.Spotify(auth=spotify_token)
 	sp.trace = False
@@ -12,19 +19,23 @@ def create_playlist(spotify_token, playlist_name):
 	with open('playlist_return.json', 'w', encoding='utf-8') as f:
 		json.dump(playlist, f, indent = 4)
 
-def get_playlists(spotify_token, spotify_username):
+def show_playlists(spotify_token, spotify_username):
 	sp = spotipy.Spotify(auth=spotify_token)
-	sp.trace = False
 	playlists = sp.user_playlists(spotify_username)
 
 	playlists_list = []
 	while playlists:
-		for i, playlist in enumerate(playlists['items']):
+		for playlist in playlists['items']:
 			playlists_list.append((playlist['uri'], playlist['name']))
-			print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'], playlist['name']))
+			# print("%s %s" % (playlist['uri'], playlist['name']))
 		if playlists['next']:
 			playlists = sp.next(playlists)
 		else:
 			playlists = None
 
 	return playlists_list
+
+def add_song(spotify_token, playlist_id, song_id):
+	sp = spotipy.Spotify(auth=spotify_token)
+	sp.trace = False
+	sp.user_playlist_add_tracks(config.spotify_username, playlist_id, [song_id])
